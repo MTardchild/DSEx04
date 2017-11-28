d0651707843facfba8930c8ee11a8d5e8b8b26a9
MTardix <Marius.Tuschl@googlemail.com>
2017-11-28 23:50:54 +0100

    Fixed my wrong implementation boys! tell everyone im back from the dead

--- DSEx04.asm
+++ DSEx04.asm
@@ -1,22 +1,34 @@
 %include "io.inc"
 
 section .data
-A DD 255h
-B DD 255h
+A DD 0ABCDEFh
+B DD 0FFFFFFFFh
+RES DQ 0
 
-%macro MULTIPLY 2
+%macro PRINT_Q 1    
+    PRINT_HEX 4, [%1]
+    PRINT_HEX 4, [%1+4]
+%endmacro
+
+%macro MULTIPLY 3
     xor eax, eax
+    xor ecx, ecx
     mov ebx, [%2]
     CALM_AND_COLLECTED_MULTIPLICATION:
-    add eax, [%1]
-    sub ebx, 1
-    jnz CALM_AND_COLLECTED_MULTIPLICATION
-    PRINT_HEX 4, eax
+    adc eax, [%1]
+    jnc NO_ONE_CARRIES_YOU_TODAY_SCRUB
+    add ecx, 1
+    NO_ONE_CARRIES_YOU_TODAY_SCRUB:
+    dec ebx
+    jnz CALM_AND_COLLECTED_MULTIPLICATION  
+    mov [%3], ecx
+    mov [%3+4], eax
+    PRINT_Q RES
 %endmacro
 
 section .text
 global CMAIN
 CMAIN:
-    MULTIPLY A, B
-    xor eax, eax
+    mov ebp, esp ;for correct debugging
+    MULTIPLY A, B, RES
     ret
